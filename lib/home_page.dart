55122a79fdc2f10a1fda463de4ad6c7ddf088724
Sami0402 <samikhan0402@gmail.com>
2024-06-27 12:38:21 +0530

    First Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final days = 30;
+    final name = "Codepur";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+            child: Text("Welcome to $days Days of flutter by $name.")),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
