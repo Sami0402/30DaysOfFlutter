5331aa1cdb8a2bc9eac0eee2fc779ac9314f71d8
Sami0402 <samikhan0402@gmail.com>
2024-07-04 11:27:34 +0530

    Day 9 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Item{
+  final String id ;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image});
+
+  
+}
+
+final products = [
+  Item(id: , name: name, desc: desc, price: price, color: color, image: image)
+]
